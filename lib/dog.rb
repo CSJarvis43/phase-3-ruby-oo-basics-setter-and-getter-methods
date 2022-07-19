32761d55e5ab00d495ad6bcf0ca2961209ef55b0
Charles Jarvis <csjarvis43@gmail.com>
2022-07-19 14:43:10 -0600

    commit tree 53a25132e0dd583e0fc67287e8d29879eeb73dc0

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,30 @@
-class Dog
+require 'pry'
 
+class Dog
+    # attr_accessor :name
+    # attr_accessor :breed
+
+    # def initialize(name, breed)
+    #     @name = name
+    #     @breed = breed
+    # end
+
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
 end
+
+# d1 = Dog.new("sam", "lab")
+# binding.pry
